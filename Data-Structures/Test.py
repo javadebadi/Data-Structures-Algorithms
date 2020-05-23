dda92427a18f4f86b168e796a770977b18fbdf7e
javadebadi <javad.ebadi.1990@gmail.com>
2020-05-23 04:41:14 -0700

    Add: Added Data-Structures/Test.py file to test new data structures

---
+++ Data-Structures/Test.py
@@ -0,0 +1,10 @@
+from LinkedList import *
+
+
+print(" ===== Test LinkedList =====")
+linked_list = LinkedList()
+linked_list.append(0.2)
+linked_list.append(1)
+linked_list.append(0)
+# print elements of the linked list
+linked_list.print_all()
