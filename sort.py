4f3a5b15af40be9fc4268c8e2fe8a2e0514d490c
javadebadi <javad.ebadi.1990@gmail.com>
2020-05-30 06:37:19 -0700

    Update: implemented a test function in sort.py to test code of other algorithms in future faster

--- sort.py
+++ sort.py
@@ -1,5 +1,6 @@
 # compare sort algorithms
-
+# import packages
+from time import time
 # selection sort
 def selectionSort(target_list):
     """Takes a list as an input and sorts it using selection sort algorihtm
@@ -21,10 +22,32 @@ def selectionSort(target_list):
                 target_list[i] = target_list[j]
                 target_list[j] = temp
 
-# test 
-test_list = range(100,-1,-1)
-print("list: ")
-print(test_list)
-selectionSort(test_list)
-print("list sorted via selection sort algorithms: ")
-print(test_list)
+# test function
+def test_sort_algorithm(function, test_list = range(10,-1,-1)):
+    """Tests a selection sort algorihtm and returns the time
+
+    Args:
+        function: the sort algorihtm which we want to test
+        test_list (list): a list which will be used for testing
+
+    Returns:
+        sort_time = time spent to sort
+
+    Time complexity:
+        time complexity of function algortim
+    """
+
+    test_list = test_list
+    print("list: ")
+    print(test_list)
+    tic = time()
+    function(test_list)
+    toc = time()
+    print("list sorted via selection sort algorithms: ")
+    print(test_list)
+
+    sort_time = (toc - tic)
+    return sort_time
+
+# test sort algortims
+print(test_sort_algorithm(selectionSort, range(10000,-1,-1)))
