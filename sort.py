cd8ae394e16a2472d9947e84823d5482de31d702
javadebadi <javad.ebadi.1990@gmail.com>
2020-05-30 07:02:34 -0700

    Update: Feature add of verbose to test_sort_algorithm function Added some codes to find computation time for lists of different lengths

--- sort.py
+++ sort.py
@@ -23,12 +23,14 @@ def selectionSort(target_list):
                 target_list[j] = temp
 
 # test function
-def test_sort_algorithm(function, test_list = range(10,-1,-1)):
+def test_sort_algorithm(function, test_list = range(10,-1,-1), verbose = False):
     """Tests a selection sort algorihtm and returns the time
 
     Args:
         function: the sort algorihtm which we want to test
         test_list (list): a list which will be used for testing
+        verbose (bool): prints information to the screen depending on its value
+            if verbose = True prints list before and after sort
 
     Returns:
         sort_time = time spent to sort
@@ -38,16 +40,29 @@ def test_sort_algorithm(function, test_list = range(10,-1,-1)):
     """
 
     test_list = test_list
-    print("list: ")
-    print(test_list)
+    if verbose:
+        print(" ===> list before sort: ")
+        print(test_list)
     tic = time()
     function(test_list)
     toc = time()
-    print("list sorted via selection sort algorithms: ")
-    print(test_list)
+    if verbose:
+        print("list sorted via selection sort algorithms: ")
+        print(test_list)
 
     sort_time = (toc - tic)
     return sort_time
 
+# dictionary of algorithm
+algorithms = dict()
+algorithms["selection sort"] = (selectionSort, {})
+
 # test sort algortims
-print(test_sort_algorithm(selectionSort, range(10000,-1,-1)))
+n = [10,100,1000,2000]
+for i in n:
+    for _ , (algorithm_func , sort_times) in algorithms.items():
+        test_list = range(i,-1,-1)
+        sort_time = test_sort_algorithm(algorithm_func, test_list)
+        sort_times[i] = sort_time
+
+print(algorithms.values())
